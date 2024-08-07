700037e696787aedb962071f3891a52d3a175b84
BorakL <lborak88@gmail.com>
2024-08-07 21:59:16 +0200

    remove comment

--- src/server.ts
+++ src/server.ts
@@ -1,28 +1,3 @@
-// const http = require('http')
-// const dotenv = require('dotenv')
-// const express = require("express");
-// import { Express, Request, Response, NextFunction} from 'express'; 
-// import bodyParser from 'body-parser';
-// dotenv.config();
-
-// const app:Express = express();
-
-// const port = process.env.PORT || 3000;
-
-// app.use(express.json())
-// app.use(bodyParser.urlencoded({ extended: true }));
-
-// app.use("/api/users", userRouter);
-
-// app.use((req:Request, res:Response, next:NextFunction)=>{
-//     res.send("HEllossa")
-// })
-
-// app.listen(port, ()=>{
-//     console.log(`Server is running on the port ${port}.`)
-// })
-
-
 import express, { NextFunction, Request, Response, Express, ErrorRequestHandler} from 'express' 
 import dotenv from 'dotenv'
 import gameRouter from './routes/gameRouter';
